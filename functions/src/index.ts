3ce60fe903f7a184b2f9c435c4f0dfbd4240bd35
Anne Anquetin <anne.anquetin@gmail.com>
2020-05-30 13:13:43 -0600

    add configs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,16 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions"
+import { firestore } from "firebase-admin"
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const createEvent = functions.https.onCall(async ({ formData }) => {
+  try {
+    await firestore()
+      .batch()
+      .create(firestore().collection("events").doc(), formData)
+    await firestore().batch().commit()
+  } catch (err) {
+    throw err
+  }
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import firebase from "firebase"
 import { Toolbar, makeStyles, Theme } from "@material-ui/core"
 import { Layout } from "./Layout"
 import { CalendarPage } from "./CalendarPage"
@@ -17,6 +18,19 @@ function App() {
       <Layout>
         <main className={classes.content}>
           <Toolbar />
+          <button
+            onClick={() =>
+              firebase
+                .functions()
+                .httpsCallable("createEvent")({
+                  formData: { title: "body cruch", description: "coolio" },
+                })
+                .then((res) => console.log(res))
+                .catch((err) => console.log(err))
+            }
+          >
+            hello
+          </button>
           <CalendarPage />
         </main>
       </Layout>
